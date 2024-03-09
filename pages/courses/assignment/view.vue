d332c19c63b1a4acce05ceb602b7645d25a194dc
Jessada Taengsuwan <65070041@kmitl.ac.th>
2024-03-10 04:22:51 +0700

    add icons and submit datetime

--- pages/courses/assignment/view.vue
+++ pages/courses/assignment/view.vue
@@ -29,6 +29,13 @@
 </script>
 <template>
     <div class="flex flex-col gap-4 w-full">
+        <div>
+            <button
+                @click="navigateTo(`/courses/assignment?id=${route.query.id}`)"
+                class="transition-all duration-200 ease-in-out py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">
+                <span class="material-icons-outlined">arrow_back</span>
+            </button>
+        </div>
         <div v-if="(assignments?.data?.length || 0) > 0" v-for="assign in assignments.data" class="flex flex-row justify-between border border-1 rounded-md gap-2 w-full p-4">
             <div class="flex items-center gap-2">
                 <div v-if="assign.u_avatar" class="rounded-md w-12 h-12"><img class="rounded-md aspect-square object-cover border bottom-1" :src="`/api/avatar/?u_id=${assign.u_id}`" /></div>
@@ -38,8 +45,12 @@
                 <div class="flex justify-center">{{ assign.u_firstname }} {{ assign.u_firstname }}</div>
             </div>
             <div class="flex gap-4 items-center">
-                <div class="">
-                    {{ assign.s_datetime ? 'ส่งแล้ว' : 'ยังไม่ส่ง' }}
+                <div class="flex items-center gap-2">
+                    <div class="flex flex-col items-end">
+                        {{ assign.s_datetime ? 'ส่งแล้ว' : 'ยังไม่ส่ง' }}
+                        <span class="text-sm text-slate-400" v-if="assign?.s_datetime">{{ assign.s_datetime }}</span>
+                    </div>
+                    <div class="material-icons-outlined">{{ assign.s_datetime ? 'check_circle' : 'close' }}</div>
                 </div>
                 <button
                     type="button"
